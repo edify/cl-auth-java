d06c808be3d3c610573caafe735515144ff8434a
diugalde <dua.0695@gmail.com>
2016-12-19 15:32:01 -0600

    Add Apache v2 license file and headers

--- src/main/groovy/org/commonlibrary/clauth/SAuthc1Signer.groovy
+++ src/main/groovy/org/commonlibrary/clauth/SAuthc1Signer.groovy
@@ -1,21 +1,26 @@
-package org.commonlibrary.clauth
-
 /*
- * Created by diugalde on 01/09/16.
+ * Copyright 2016 Edify Software Consulting.
  *
- * This groovy version of the SAuthc1 algorithm is based on the Stormpath Java SDK.
+ * Licensed under the Apache License, Version 2.0 (the "License").
+ * You may not use this file except in compliance with the License.
+ * A copy of the License is located at
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ *  http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
+ */
+
+
+ package org.commonlibrary.clauth
+
+/*
+ * Created by diugalde on 01/09/16.
+ *
+ * This groovy version of the SAuthc1 algorithm is based on the Stormpath Java SDK.
  */
 
 import java.text.SimpleDateFormat
--- src/main/groovy/org/commonlibrary/clauth/services/impl/ApiKeyServiceImpl.groovy
+++ src/main/groovy/org/commonlibrary/clauth/services/impl/ApiKeyServiceImpl.groovy
@@ -1,4 +1,21 @@
-package org.commonlibrary.clauth.services.impl
+/*
+ * Copyright 2016 Edify Software Consulting.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License").
+ * You may not use this file except in compliance with the License.
+ * A copy of the License is located at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+ package org.commonlibrary.clauth.services.impl
 
 import com.lambdaworks.redis.RedisClient
 import org.commonlibrary.clauth.services.ApiKeyService
@@ -20,7 +37,7 @@ class ApiKeyServiceImpl implements ApiKeyService {
         def redisHost = env['CL_REDIS_HOST'] ?: 'localhost'
         def redisPort = env['CL_REDIS_PORT'] ?: 6379
         this.redisConnection = RedisClient.create("redis://${redisHost}").connect()
-        this.decryptionPassphrase = env['CL_AUTH_PASSPHRASE']
+        this.decryptionPassphrase = env['CL_AUTH_PASSPHRASE'] ?: 'passphrase'
     }
 
     ApiKeyServiceImpl(redisConnection, decryptionPassphrase) {
